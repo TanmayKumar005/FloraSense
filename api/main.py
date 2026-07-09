0ce087463a843c2b1e63da0c4e79022f567e70cd
TanmayKumar005 <tanmay22605@gmail.com>
2026-07-09 16:16:31 +0530

    Allow Vercel frontend in CORS

--- api/main.py
+++ api/main.py
@@ -15,7 +15,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://flora-sense-three.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
